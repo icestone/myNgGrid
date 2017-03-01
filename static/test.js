37f8493eb891063ebeb79187fe754f2da96e2f31
shent <15852557449@163.com>
2017-03-01 09:35:56 +0800

    my ng-Grid

---
+++ static/test.js
@@ -0,0 +1,72 @@
+var MyNg = {
+	createTable: function(args){
+		var tableTemplate = "";
+		if(args.title){
+			args.title = "表格";
+		}
+		tableTemplate += '<div class="cl pd-5 bg-1 bk-gray mt-20">' +
+    		'<span class="l">'+args.title+'</span>';
+		
+		
+		if(args.columns){
+			var thsStr = "";
+			var tdsStr = "";
+			
+			for(var i in args.columns){
+				var column = args.columns[i];
+				thsStr += '<th>'+column.text+'</th>';
+				if(column.xtype != "actioncolumn"){
+					tdsStr += '<td>{{obj.'+column.dataIndex;
+					if(column.format){tdsStr += ' | '+column.format;}
+						tdsStr += '}}</td>';
+				}else{
+					tdsStr += '<td>'+column.actionBtns+'</td>';
+				}
+			}
+			
+			tableTemplate += '<span class="r">共有数据：<strong>{{page.totalCount}}</strong> 条</span>' +
+			'</div>' +
+	        '<table class="table table-border table-bordered table-bg">' +
+				'<thead>' +
+					'<tr class="text-c">' + 
+					thsStr + 
+					'</tr>' + 
+					'<tr class="text-c" ng-repeat="obj in lists">' + 
+					tdsStr + 
+					'</tr>' + 
+				'</thead>' + 
+			'</table>' + 
+			'<div class="dataTables_info" id="DataTables_Table_0_info" role="status" aria-live="polite">显示 {{page.startRow}} 到 {{page.endRow}} ，共 {{lists.length}} 条</div>' + 
+			'<div class="dataTables_paginate paging_simple_numbers" id="DataTables_Table_0_paginate">' + 
+				'<a class="paginate_button previous disabled" aria-controls="DataTables_Table_0" data-dt-idx="0" tabindex="0" ng-click="prePage()">上一页</a>' + 
+				'<span><a class="paginate_button current" aria-controls="DataTables_Table_0" data-dt-idx="1" tabindex="0">{{page.page}}</a></span>' + 
+				'<a class="paginate_button next disabled" aria-controls="DataTables_Table_0" data-dt-idx="2" tabindex="0" ng-click="nextPage()">下一页</a>' + 
+			'</div>';
+		}
+
+		var app = angular.module('myApp', []);
+		app.filter('formatTime', function () {
+		    return function (value) {
+		    	if (!value) return '';
+		        return new Date(value).Format("yyyy-MM-dd");
+		    };
+		}).filter('formatZt', function () {
+		    return function (value) {
+		    	if (!value) return '';
+		    	if(value == 1) return "待审核";
+		    	if(value == 2) return "预审通过";
+		    	if(value == 3) return "审核不通过";
+		    	if(value == 4) return "审核通过";
+		    };
+		}).directive('mytable', [ function(){
+		    return {
+		        // scope: false, // 默认值，共享父级作用域
+		        // controller: function($scope, $element, $attrs, $transclude) {},
+		        restrict: 'AE', // E = Element, A = Attribute, C = Class, M = Comment
+		        template: tableTemplate
+		    };
+		}]);
+		
+		return app;
+	}
+}
